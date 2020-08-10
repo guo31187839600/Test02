ad2fbf45894a60c13a782e1bba61b2f9e37e36e6
guojinren <3118783960@qq.com>
2020-08-10 12:11:08 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author guojinren
+ * @be created at 2020-08-10-11:58
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
